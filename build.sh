d4e9556297e0ed881700487dab13364980a1789d
SantiagoMontanaCastro <sadm29375@gmail.com>
2024-10-11 00:17:42 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,7 @@ set -o errexit
 
 pip install -r requirements.txt
 
-# Convert static asset files
-python manage.py collectstatic
+
 
 # Apply any outstanding database migrations
 python manage.py migrate
