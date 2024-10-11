2f6943d2f17078b70691823a2347d26226c48113
SantiagoMontanaCastro <sadm29375@gmail.com>
2024-10-11 00:38:13 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 # Exit on error
 set -o errexit
 
-pip install -r requirements.txt
+# poetry install
+# pip install -r requirements.txt
 
 
 python manage.py collectstatic --no-input
-# Apply any outstanding database migrations
 python manage.py migrate
